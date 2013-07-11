9529d6132e19df1752f0ef8ad6b474d3cdfa4ede
Kaiser Franz Josef <wecodemore@gmail.com>
2013-07-11 18:45:16 +0200

    Reduzed file size/length; Changed comma style; Increased Version Nr. to 0.7.

--- dynamic_img_resize.php
+++ dynamic_img_resize.php
@@ -1,25 +1,22 @@
 <?php
+defined( 'ABSPATH' ) OR exit;
 /**
  * Plugin Name: Dynamic Image resize
  * Plugin URI:  http://unserkaiser.com/plugins/dynamic-image-resize/
- * Description: Dynamically resizes the image. Enables the [dynamic_image] shortcode, pseudo-TimThumb but creates resized and cropped image files from existing media library entries. Usage: <code>[dynamic_image src="http://example.org/wp-content/uploads/2012/03/image.png" width="100" height="100"]</code>. Also offers a template tag.
- * Version:     0.6.2
+ * Description: Dynamically resizes images. Enables the <code>[dynamic_image]</code> shortcode, pseudo-TimThumb but creates resized and cropped image files from existing media library entries. Usage: <code>[dynamic_image src="http://example.org/wp-content/uploads/2012/03/image.png" width="100" height="100"]</code>. Also offers a template tag.
+ * Version:     0.7
  * Author:      Franz Josef Kaiser <http://unserkaiser.com/contact/>
  * Author URI:  http://unserkaiser.com
  * License:     MIT
  */
-// Prevent loading this file directly - Busted!
-defined( 'ABSPATH' ) OR exit;
 
 
 if ( ! class_exists( 'oxoDynamicImageResize' ) )
 {
-
 /**
  * @author Franz Josef Kaiser
  * @link http://unserkaiser.com
  * @license MIT
- *
  * @since 0.2
  */
 class oxoDynamicImageResize
@@ -30,37 +27,30 @@ class oxoDynamicImageResize
 	 */
 	public $atts;
 
-
 	/**
 	 * Constructor
 	 * Adds the shortcode
-	 *
 	 * @since 0.2
-	 *
 	 * @param array $atts
-	 *
-	 * @return void
+	 * @return \oxoDynamicImageResize
 	 */
 	public function __construct( $atts )
 	{
 		if ( ! is_array( $atts ) )
 		{
 			return new WP_Error(
-				 'wrong_arg_type'
-				,__( 'Arguments need to be an array.', 'dyn_textdomain' )
-				,__FILE__
+				'wrong_arg_type',
+				__( 'Arguments need to be an array.', 'dyn_textdomain' ),
+				__FILE__
 			);
 		}
 
 		$this->atts = $this->sanitize( $atts );
 	}
 
-
 	/**
 	 * Returns the image
-	 *
 	 * @since 0.2
-	 *
 	 * @return string $output Image Html Mark-Up or Error (Guest/Subscriber get an empty string)
 	 */
 	public function __toString()
@@ -84,22 +74,18 @@ class oxoDynamicImageResize
 		return "{$output->get_error_message( 'no_attachment' )}: {$output->get_error_data()}";
 	}
 
-
 	/**
 	 * Sanitize attributes
-	 *
 	 * @since 0.5
-	 *
 	 * @param array $atts
-	 *
 	 * @return array $atts
 	 */
 	public function sanitize( $atts )
 	{
 		// Get rid of eventual leading/trailing white spaces around atts
 		$atts = array_map(
-			 'trim'
-			,$atts
+			'trim',
+			$atts
 		);
 
 		# >>>> Sanitize
@@ -115,29 +101,24 @@ class oxoDynamicImageResize
 		return $atts;
 	}
 
-
 	/**
 	 * Builds the image
-	 *
-	 * @since 0.1
-	 *
- 	 * @uses image_make_intermediate_size
-	 *
-	 * @param array $atts
-	 *
-	 * @return mixed string/WP Error $html
+	 * @since    0.1
+	 * @uses     image_make_intermediate_size
+	 * @internal param array $atts
+	 * @return   mixed string/WP Error $html
 	 */
 	public function get_image()
 	{
 		// parse defaults/attributes
 		extract( shortcode_atts(
-			 array(
-				 'src'     => ''
-				,'width'   => ''
-				,'height'  => ''
-				,'classes' => ''
-			 )
-			,$this->atts
+			array(
+				'src'     => '',
+				'width'   => '',
+				'height'  => '',
+				'classes' => '',
+			),
+			$this->atts
 		), EXTR_SKIP );
 
 		$hw_string    = image_hwstring( $width, $height );
@@ -163,25 +144,25 @@ class oxoDynamicImageResize
 
 			// Let's see if the image belongs to our uploads directory...
 			$img_url = substr(
-				 $src
-				,0
-				,strlen( $base_url )
+				$src,
+				0,
+				strlen( $base_url )
 			);
 			// ...And if not: just return the image HTML string
 			if ( $img_url !== $base_url )
 			{
 				return $this->get_markup(
-					 $img_url
-					,$hw_string
-					,$classes
+					$img_url,
+					$hw_string,
+					$classes
 				);
 			}
 
 			// Look up the file in the database.
 			$file   = str_replace(
-				 trailingslashit( $base_url )
-				,''
-				,$src
+				trailingslashit( $base_url ),
+				'',
+				$src
 			);
 			$att_id = $this->get_attachment( $file );
 
@@ -203,9 +184,9 @@ class oxoDynamicImageResize
 			# @TODO In case, we got an ID, but found no image: if ( ! $src ) $file = $att_id;
 
 			return new WP_Error(
-				 'no_attachment'
-				,__( 'Attachment not found.', 'dyn_textdomain' )
-				,$file
+				'no_attachment',
+				__( 'Attachment not found.', 'dyn_textdomain' ),
+				$file
 			);
 		}
 
@@ -219,9 +200,9 @@ class oxoDynamicImageResize
 				)
 			{
 				$src = str_replace(
-					 basename( $src )
-					,$size['file']
-					,$src
+					basename( $src ),
+					$size['file'],
+					$src
 				);
 				$needs_resize = false;
 				break;
@@ -234,25 +215,25 @@ class oxoDynamicImageResize
 			$attached_file = get_attached_file( $att_id );
 			// ...we can create one.
 			$resized       = image_make_intermediate_size(
-				 $attached_file
-				,$width
-				,$height
-				,true
+				$attached_file,
+				$width,
+				$height,
+				true
 			);
 
 			if ( ! is_wp_error( $resized ) )
 			{
 				// Let metadata know about our new size.
 				$key = sprintf(
-					 'resized-%dx%d'
-					,$width
-					,$height
+					'resized-%dx%d',
+					$width,
+					$height
 				);
 				$meta['sizes'][ $key ] = $resized;
 				$src = str_replace(
-					 basename( $src )
-					,$resized['file']
-					,$src
+					basename( $src ),
+					$resized['file'],
+					$src
 				);
 
 				wp_update_attachment_metadata( $att_id, $meta );
@@ -260,9 +241,9 @@ class oxoDynamicImageResize
 				// Record in backup sizes, so everything's
 				// cleaned up when attachment is deleted.
 				$backup_sizes = get_post_meta(
-					 $att_id
-					,'_wp_attachment_backup_sizes'
-					,true
+					$att_id,
+					'_wp_attachment_backup_sizes',
+					true
 				);
 
 				! is_array( $backup_sizes ) AND $backup_sizes = array();
@@ -270,31 +251,27 @@ class oxoDynamicImageResize
 				$backup_sizes[ $key ] = $resized;
 
 				update_post_meta(
-					 $att_id
-					,'_wp_attachment_backup_sizes'
-					,$backup_sizes
+					$att_id,
+					'_wp_attachment_backup_sizes',
+					$backup_sizes
 				);
 			}
 		}
 
 		// Generate the markup and return:
 		$html = $this->get_markup(
-			 $src
-			,$hw_string
-			,$classes
+			$src,
+			$hw_string,
+			$classes
 		);
 
 	 	return $html;
 	}
 
-
 	/**
 	 * Query for the file by URl
-	 *
 	 * @since 0.2
-	 *
 	 * @param string $file
-	 *
 	 * @return mixed string/bool $result Attachment or FALSE if nothing was found (needed for error)
 	 */
 	public function get_attachment( $file )
@@ -303,15 +280,15 @@ class oxoDynamicImageResize
 
 		$file   = like_escape( $file );
 		$result = $wpdb->get_var( $wpdb->prepare(
-			 "
+			"
 				SELECT post_id
 				FROM {$wpdb->postmeta}
 				WHERE meta_key = '_wp_attachment_metadata'
 				  AND meta_value
 				  LIKE %s
 				LIMIT 1;
-			 "
-			,"%{$file}%"
+			",
+			"%{$file}%"
 		) );
 
 		// FALSE if no result
@@ -321,25 +298,21 @@ class oxoDynamicImageResize
 		return $result;
 	}
 
-
 	/**
 	 * Builds the markup
-	 *
 	 * @since 0.2
-	 *
 	 * @param string $src URl to the image
 	 * @param string $hw_string
 	 * @param string $classes
-	 *
 	 * @return string $html
 	 */
 	public function get_markup( $src, $hw_string, $classes )
 	{
 		return sprintf(
-			 '<img src="%s" %s %s />'
-			,$src
-			,$hw_string
-			,"class='{$classes}'"
+			'<img src="%s" %s %s />',
+			$src,
+			$hw_string,
+			"class='{$classes}'"
 		);
 	}
 } // END Class oxoDynamicImageResize
@@ -352,11 +325,8 @@ class oxoDynamicImageResize
 
 /**
  * Retrieve a dynamically/on-the-fly resized image
- *
  * @since 0.2
- *
  * @param array $atts Attributes: src(URi/ID), width, height, classes
- *
  * @return mixed string/html $html Image mark up
  */
 function dynamic_image_resize( $atts )
